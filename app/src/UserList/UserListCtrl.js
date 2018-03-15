474e7365242da2bf7d23650f2f007cf0f75d7876
Alexander Doloz <doloz@outlook.com>
2018-03-15 20:04:18 +0300

    Add loaders

--- app/src/UserList/UserListCtrl.js
+++ app/src/UserList/UserListCtrl.js
@@ -1,11 +1,15 @@
 'use strict'
 
 userApp.controller('UserListCtrl', function ($scope, UsersService, PostsService) {
+  $scope.usersLoading = true;
   UsersService.getUsers().then(function (response) {
+    $scope.usersLoading = false;
     $scope.users = response.data
   })
 
+  $scope.postsLoading = true;
   PostsService.getPosts().then(function (response) {
+    $scope.postsLoading = false;
     $scope.posts = response.data
   })
 
